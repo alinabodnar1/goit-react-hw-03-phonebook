d73e71e272a617a12e379022af81cc0a5e35969b
alinabodnar1 <malina.bodnar@gmail.com>
2023-05-12 09:08:39 +0300

    commit to check live page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -73,7 +73,7 @@ export default class App extends Component {
     }));
  }
   
-  //---------------------------------------------------------------
+  
   render() {
     
     const { filter, isCreated, isDelete} = this.state;
